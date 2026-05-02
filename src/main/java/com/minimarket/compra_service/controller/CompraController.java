c9fc6d809f6021a65eef22faf18d105cac4f9d66
neurott <neurott@protonmail.com>
2026-05-02 17:42:50 -0400

    Falta el webclient nomás

--- src/main/java/com/minimarket/compra_service/controller/CompraController.java
+++ src/main/java/com/minimarket/compra_service/controller/CompraController.java
@@ -1,13 +1,16 @@
 package com.minimarket.compra_service.controller;
 
 
+import com.minimarket.compra_service.dto.CompraRequestDTO;
 import com.minimarket.compra_service.dto.CompraResponseDTO;
+import com.minimarket.compra_service.model.Compra;
+import com.minimarket.compra_service.model.EstadoCompra;
 import com.minimarket.compra_service.service.CompraService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,6 +25,39 @@ public class CompraController {
     public ResponseEntity<List<CompraResponseDTO>> listar(){
         return ResponseEntity.ok(compraService.obtenerTodos());
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CompraResponseDTO> obtenerPorId(@PathVariable Long id){
+        return compraService.obtenerPorId(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/proveedor/{proveedorId}")
+    public ResponseEntity<List<CompraResponseDTO>> obtenerPorProveedor(@PathVariable Long proveedorId){
+        return ResponseEntity.ok(compraService.obtenerPorProveedor(proveedorId));
+    }
+
+    @PostMapping
+    public ResponseEntity<CompraResponseDTO> guardarCompra(@Valid @RequestBody CompraRequestDTO dto){
+        return ResponseEntity.status(201).body(compraService.guardar(dto));
+    }
+
+    //no put, pero si patch, pq usamos un puro dato nomás, mejor un requestParam
+    @PatchMapping("{id}/estado")
+    public ResponseEntity<CompraResponseDTO> actualizarEstado(@PathVariable Long id, @RequestParam EstadoCompra estado){
+        return compraService.actualizarEstado(id,estado).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    // eliminar
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminar(@PathVariable Long id){
+        compraService.eliminarCompra(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+
+
+
 
 
 }
