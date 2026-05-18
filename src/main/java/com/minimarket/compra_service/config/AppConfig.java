e6726b950d8a0866c3058e4f4c19279e1920f26a
neurott <neurott@protonmail.com>
2026-05-17 20:24:36 -0400

    testing

---
+++ src/main/java/com/minimarket/compra_service/config/AppConfig.java
@@ -0,0 +1,19 @@
+package com.minimarket.compra_service.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class AppConfig {
+
+    @Value("${proveedor-service.url}")
+    public String proveedorUrl;
+
+
+    @Bean
+    public WebClient webClient(){
+        return WebClient.builder().baseUrl(proveedorUrl).build();
+    }
+}
--- src/main/java/com/minimarket/compra_service/service/CompraService.java
+++ src/main/java/com/minimarket/compra_service/service/CompraService.java
@@ -8,23 +8,26 @@ import com.minimarket.compra_service.model.Compra;
 import com.minimarket.compra_service.model.DetalleCompra;
 import com.minimarket.compra_service.model.EstadoCompra;
 import com.minimarket.compra_service.repository.CompraRepository;
-import com.minimarket.compra_service.webclient.ProveedorClient;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class CompraService {
 
     private final CompraRepository compraRepository;
-    private final ProveedorClient proveedorClient;
+    private final WebClient webClient;
 
     public CompraResponseDTO mapToDto(Compra compra){
         List< DetalleCompraResponseDTO> detalles = compra.getDetalles().stream().
@@ -41,6 +44,18 @@ public class CompraService {
                 compra.getEstado().name(),
                 detalles
         );
+    }
+
+    public void validarProveedorId(Long proveedorId){
+        try{
+            webClient.get().uri("/api/v1/proveedores/{id}", proveedorId).retrieve().bodyToMono(String.class).block();
+            log.info("EL PROVEEDOR CON EL ID {} HA SIDO VALIDADO CORRECAMENTE(WEBCLIENT)", proveedorId);
+
+        }catch (WebClientResponseException.NotFound we){
+            throw new RuntimeException("EL PROVEEDOR CON EL ID: " + proveedorId + "NO EXISTE EN EL PROVEEDOR-SERVICE");
+        }catch(Exception e){
+            throw new RuntimeException("NO SE PUDO CONECTAR CON EL PROVEEDOR-SERVICE: " + e.getMessage());
+        }
     }
 
     //detalles
@@ -64,7 +79,7 @@ public class CompraService {
 
     public CompraResponseDTO guardar (CompraRequestDTO dto){
 
-        proveedorClient.obtenerProveedorId(dto.getProveedorId());
+        validarProveedorId(dto.getProveedorId());
 
         Compra compra = new Compra(null, dto.getProveedorId(), LocalDateTime.now(), 0.0, EstadoCompra.PENDIENTE, null, true);
         List<DetalleCompra> detalles = dto.getDetalles().stream().map( d -> mapToDetalle(d,compra)).toList();
